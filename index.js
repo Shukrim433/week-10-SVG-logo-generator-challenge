a1572458e81451906703d61a8f1caef74fadd57c
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-03-23 22:37:41 +0000

    imported rhe CLI module, created a new instance of the class and called the run method.

---
+++ index.js
@@ -0,0 +1,5 @@
+const CLI = require('./lib/cli') //importing the CLI module/file
+
+const cli = new CLI() //creating a new object/instance of the CLI class
+
+cli.run() //invoking the run() method on the new cli object/instance (method inherited from the CLI class)
