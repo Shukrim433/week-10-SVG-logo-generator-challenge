9ff7a9fba6632e51feed0fef8f8cd43e80f6a3fb
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-03-24 14:36:22 +0000

    changed the names of the test suites to make them more semantic

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,20 +1,20 @@
 //tests to check if colour entered for each shape subclass is equal to the one generated in the markup.
 const {Circle, Triangle, Square} = require('../lib/shapes')
-    describe('Circle colour test', () => {
+    describe('Circle colour markup test', () => {
         it('should render the circle svg  markup with the colour the user entered.', () =>{
             const expectedMarkup =  `<circle cx="150" cy="100" r="80" fill="blue" />`//what we expect the code to look like when the user enters blue as the colour 
             const markupWithUserColour = new Circle('blue') //section of code from the actual code. passing in the colour 'blue'
             expect(markupWithUserColour.render()).toEqual(expectedMarkup)
         })
     })
-    describe('Square colour test', () => {
+    describe('Square colour markup test', () => {
         it('should render the square svg  markup with the colour the user entered.', () =>{
             const expectedMarkup = `<rect x="90" y="40" width="120" height="120" fill="pink" />`//what we expect the code to look like when the user enters blue as the colour 
             const markupWithUserColour = new Square('pink') //section of code from the actual code. passing in the colour 'blue'
             expect(markupWithUserColour.render()).toEqual(expectedMarkup)
         })
     })
-    describe('Triangle colour test', () => {
+    describe('Triangle colour markup test', () => {
         it('should render the triangle svg  markup with the colour the user entered.', () =>{
             const expectedMarkup = `<polygon points="150,20 250,180 50,180" fill="green" />`//what we expect the code to look like when the user enters blue as the colour 
             const markupWithUserColour = new Triangle('green') //section of code from the actual code. passing in the colour 'blue'
@@ -27,5 +27,4 @@ const {Circle, Triangle, Square} = require('../lib/shapes')
     //So no need to test the 'typeOf' of the colour entered.
 
     
-    //test to check if the hex code a user enters is valid if not it throws an error:
-    
+
